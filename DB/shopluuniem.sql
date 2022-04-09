4f39ea09d52077c609df5de922f3fee4a8773a0a
lethanh010820 <lethanh010820@gmail.com>
2022-04-10 01:22:18 +0700

    database finish

--- DB/shopluuniem.sql
+++ DB/shopluuniem.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Máy chủ: 127.0.0.1
--- Thời gian đã tạo: Th4 04, 2022 lúc 03:10 PM
+-- Thời gian đã tạo: Th4 09, 2022 lúc 08:21 PM
 -- Phiên bản máy phục vụ: 10.4.22-MariaDB
 -- Phiên bản PHP: 7.4.27
 
@@ -40,7 +40,8 @@ CREATE TABLE `bill` (
 --
 
 INSERT INTO `bill` (`id`, `userId`, `createdDate`, `receivedDate`, `status`) VALUES
-(22, 11, '2022-04-01', NULL, 'Processing');
+(26, 17, '2022-04-09', '2022-04-09', 'Complete'),
+(27, 17, '2022-04-09', '2022-04-09', 'Complete');
 
 -- --------------------------------------------------------
 
@@ -63,7 +64,9 @@ CREATE TABLE `billdetails` (
 --
 
 INSERT INTO `billdetails` (`id`, `orderId`, `productId`, `quantity`, `productPrice`, `productName`, `productImage`) VALUES
-(12, 22, 1, 2, '20000', 'Cốc đẹp nè', 'coc-su-hai-tac-3.jpg');
+(17, 26, 22, 1, '500000', 'Đèn mặt trăng', 'denmattrang.jpg'),
+(18, 27, 22, 2, '500000', 'Đèn mặt trăng', 'denmattrang.jpg'),
+(19, 27, 21, 1, '4500000', 'Đèn lồng kính', 'denlongkinh.jpg');
 
 -- --------------------------------------------------------
 
@@ -80,14 +83,6 @@ CREATE TABLE `cart` (
   `giaBan` int(11) NOT NULL,
   `image` varchar(255) COLLATE utf8_unicode_ci NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci;
-
---
--- Đang đổ dữ liệu cho bảng `cart`
---
-
-INSERT INTO `cart` (`Id`, `userId`, `maSP`, `quantity`, `tenSP`, `giaBan`, `image`) VALUES
-(9, '9', 3, 1, 'Coc 22', 30000, 'coc-su-hai-tac-3.jpg'),
-(14, '9', 4, 2, 'Coc24142', 30000, 'a');
 
 -- --------------------------------------------------------
 
@@ -120,7 +115,7 @@ INSERT INTO `loaisanpham` (`maLoai`, `tenLoai`) VALUES
 (1, 'Cốc sứ'),
 (2, 'Đèn ngủ'),
 (3, 'Bật lửa'),
-(4, 'Khung ảnh'),
+(4, 'keo'),
 (5, 'Đồ chơi');
 
 -- --------------------------------------------------------
@@ -164,10 +159,36 @@ CREATE TABLE `sanpham` (
 --
 
 INSERT INTO `sanpham` (`maSP`, `tenSP`, `image`, `giaBan`, `quantity`, `dvt`, `maLoai`, `soldcount`) VALUES
-(1, 'Cốc đẹp nè', 'coc-su-hai-tac-3.jpg', 20000, 995, 'cốc Mô tả sản phẩmMô tả sản phẩmMô tả sản phẩmMô tả sản phẩmMô tả sản phẩmMô tả sản phẩmMô tả sản phẩmMô tả sản phẩmMô tả sản phẩmMô tả sản phẩm', 1, 5),
-(2, 'Coc 2', 'coc-su-hai-tac-3.jpg', 30000, 45, 'hellllo', 2, 0),
-(3, 'Coc 22', 'coc-su-hai-tac-3.jpg', 30000, 551, 'hellllo', 2, 4),
-(4, 'Coc 24142', 'coc-su-hai-tac-3.jpg', 30000, 444, 'hellll4o', 2, 0);
+(5, 'Bật lửa quý ông', 'batlua1.jpg', 1500000, 100, 'Quả cầu tuyết giáng sinh ấm áp có kích thước 12 x 16 cm.\r\n\r\nPhát nhạc bằng cót, có đèn đổi màu ở cả chân đế và quả cầu.\r\n\r\nKhi phát nhạc, nội thất trong quả cầu có thể xoay tròn và tuyết tự phun mà không cần phải lắc.\r\n\r\nĐây là mẫu quả cầu tuyết cao cấp nên các chi tiết đều được làm vô cùng sắc nét và tinh tế.\r\n\r\nMẫu quả cầu tuyết này có số lượng giới hạn nên hãy nhanh tay đặt hàng nhé.', 3, 0),
+(6, 'Bật lửa 1', 'batlua2.jpg', 200000, 901, 'Quả cầu tuyết giáng sinh ấm áp có kích thước 12 x 16 cm.\r\n\r\nPhát nhạc bằng cót, có đèn đổi màu ở cả chân đế và quả cầu.\r\n\r\nKhi phát nhạc, nội thất trong quả cầu có thể xoay tròn và tuyết tự phun mà không cần phải lắc.\r\n\r\nĐây là mẫu quả cầu tuyết cao cấp nên các chi tiết đều được làm vô cùng sắc nét và tinh tế.\r\n\r\nMẫu quả cầu tuyết này có số lượng giới hạn nên hãy nhanh tay đặt hàng nhé.', 1, 0),
+(7, 'Bật lửa hình lựu đạn', 'batlua3.jpg', 4500000, 101, 'Quả cầu tuyết giáng sinh ấm áp có kích thước 12 x 16 cm.\r\n\r\nPhát nhạc bằng cót, có đèn đổi màu ở cả chân đế và quả cầu.\r\n\r\nKhi phát nhạc, nội thất trong quả cầu có thể xoay tròn và tuyết tự phun mà không cần phải lắc.\r\n\r\nĐây là mẫu quả cầu tuyết cao cấp nên các chi tiết đều được làm vô cùng sắc nét và tinh tế.\r\n\r\nMẫu quả cầu tuyết này có số lượng giới hạn nên hãy nhanh tay đặt hàng nhé.', 1, 0),
+(8, 'Bật lửa nghệ thuật new', '36baf5f506.jpg', 150000, 100, 'Quả cầu tuyết giáng sinh ấm áp có kích thước 12 x 16 cm.', 1, 0),
+(9, 'Bật lửa Ý', 'batlua5.jpg', 4500000, 100, 'Quả cầu tuyết giáng sinh ấm áp có kích thước 12 x 16 cm.\r\n\r\nPhát nhạc bằng cót, có đèn đổi màu ở cả chân đế và quả cầu.\r\n\r\nKhi phát nhạc, nội thất trong quả cầu có thể xoay tròn và tuyết tự phun mà không cần phải lắc.\r\n\r\nĐây là mẫu quả cầu tuyết cao cấp nên các chi tiết đều được làm vô cùng sắc nét và tinh tế.\r\n\r\nMẫu quả cầu tuyết này có số lượng giới hạn nên hãy nhanh tay đặt hàng nhé.', 3, 0),
+(10, 'Bật lửa sáng tạo 2', 'batlua6.jpg', 20000, 90, 'Quả cầu tuyết giáng sinh ấm áp có kích thước 12 x 16 cm.\r\n\r\nPhát nhạc bằng cót, có đèn đổi màu ở cả chân đế và quả cầu.\r\n\r\nKhi phát nhạc, nội thất trong quả cầu có thể xoay tròn và tuyết tự phun mà không cần phải lắc.\r\n\r\nĐây là mẫu quả cầu tuyết cao cấp nên các chi tiết đều được làm vô cùng sắc nét và tinh tế.\r\n\r\nMẫu quả cầu tuyết này có số lượng giới hạn nên hãy nhanh tay đặt hàng nhé.', 1, 0),
+(11, 'Cầu tuyết', 'cautuyet.jpg', 50000, 100, 'Quả cầu tuyết giáng sinh ấm áp có kích thước 12 x 16 cm.\r\n\r\nPhát nhạc bằng cót, có đèn đổi màu ở cả chân đế và quả cầu.\r\n\r\nKhi phát nhạc, nội thất trong quả cầu có thể xoay tròn và tuyết tự phun mà không cần phải lắc.\r\n\r\nĐây là mẫu quả cầu tuyết cao cấp nên các chi tiết đều được làm vô cùng sắc nét và tinh tế.\r\n\r\nMẫu quả cầu tuyết này có số lượng giới hạn nên hãy nhanh tay đặt hàng nhé.', 5, 0),
+(12, 'Hộp tiết kiệm', 'hoptietkiem.jpg', 250000, 80, 'Quả cầu tuyết giáng sinh ấm áp có kích thước 12 x 16 cm.\r\n\r\nPhát nhạc bằng cót, có đèn đổi màu ở cả chân đế và quả cầu.\r\n\r\nKhi phát nhạc, nội thất trong quả cầu có thể xoay tròn và tuyết tự phun mà không cần phải lắc.\r\n\r\nĐây là mẫu quả cầu tuyết cao cấp nên các chi tiết đều được làm vô cùng sắc nét và tinh tế.\r\n\r\nMẫu quả cầu tuyết này có số lượng giới hạn nên hãy nhanh tay đặt hàng nhé.', 1, 0),
+(13, 'Hộp nhạc', 'hopnhac.jpg', 450000, 100, 'Quả cầu tuyết giáng sinh ấm áp có kích thước 12 x 16 cm.\r\n\r\nPhát nhạc bằng cót, có đèn đổi màu ở cả chân đế và quả cầu.\r\n\r\nKhi phát nhạc, nội thất trong quả cầu có thể xoay tròn và tuyết tự phun mà không cần phải lắc.\r\n\r\nĐây là mẫu quả cầu tuyết cao cấp nên các chi tiết đều được làm vô cùng sắc nét và tinh tế.\r\n\r\nMẫu quả cầu tuyết này có số lượng giới hạn nên hãy nhanh tay đặt hàng nhé.', 5, 0),
+(14, 'Đồng hồ cát', 'donghocat.jpg', 150000, 80, 'Quả cầu tuyết giáng sinh ấm áp có kích thước 12 x 16 cm.\r\n\r\nPhát nhạc bằng cót, có đèn đổi màu ở cả chân đế và quả cầu.\r\n\r\nKhi phát nhạc, nội thất trong quả cầu có thể xoay tròn và tuyết tự phun mà không cần phải lắc.\r\n\r\nĐây là mẫu quả cầu tuyết cao cấp nên các chi tiết đều được làm vô cùng sắc nét và tinh tế.\r\n\r\nMẫu quả cầu tuyết này có số lượng giới hạn nên hãy nhanh tay đặt hàng nhé.', 5, 0),
+(15, 'Sodoku', 'sodoku.jpg', 2000000, 100, 'Quả cầu tuyết giáng sinh ấm áp có kích thước 12 x 16 cm.\r\n\r\nPhát nhạc bằng cót, có đèn đổi màu ở cả chân đế và quả cầu.\r\n\r\nKhi phát nhạc, nội thất trong quả cầu có thể xoay tròn và tuyết tự phun mà không cần phải lắc.\r\n\r\nĐây là mẫu quả cầu tuyết cao cấp nên các chi tiết đều được làm vô cùng sắc nét và tinh tế.\r\n\r\nMẫu quả cầu tuyết này có số lượng giới hạn nên hãy nhanh tay đặt hàng nhé.', 5, 0),
+(16, 'Xếp gỗ', 'xepgo.jpg', 30000, 100, 'Quả cầu tuyết giáng sinh ấm áp có kích thước 12 x 16 cm.\r\n\r\nPhát nhạc bằng cót, có đèn đổi màu ở cả chân đế và quả cầu.\r\n\r\nKhi phát nhạc, nội thất trong quả cầu có thể xoay tròn và tuyết tự phun mà không cần phải lắc.\r\n\r\nĐây là mẫu quả cầu tuyết cao cấp nên các chi tiết đều được làm vô cùng sắc nét và tinh tế.\r\n\r\nMẫu quả cầu tuyết này có số lượng giới hạn nên hãy nhanh tay đặt hàng nhé.', 5, 0),
+(17, 'Đèn 3D', 'den3d.jpg', 500000, 100, 'Quả cầu tuyết giáng sinh ấm áp có kích thước 12 x 16 cm.\r\n\r\nPhát nhạc bằng cót, có đèn đổi màu ở cả chân đế và quả cầu.\r\n\r\nKhi phát nhạc, nội thất trong quả cầu có thể xoay tròn và tuyết tự phun mà không cần phải lắc.\r\n\r\nĐây là mẫu quả cầu tuyết cao cấp nên các chi tiết đều được làm vô cùng sắc nét và tinh tế.\r\n\r\nMẫu quả cầu tuyết này có số lượng giới hạn nên hãy nhanh tay đặt hàng nhé.', 2, 0),
+(18, 'Đèn hình con gà', 'denconga.jpg', 500000, 90, 'Quả cầu tuyết giáng sinh ấm áp có kích thước 12 x 16 cm.\r\n\r\nPhát nhạc bằng cót, có đèn đổi màu ở cả chân đế và quả cầu.\r\n\r\nKhi phát nhạc, nội thất trong quả cầu có thể xoay tròn và tuyết tự phun mà không cần phải lắc.\r\n\r\nĐây là mẫu quả cầu tuyết cao cấp nên các chi tiết đều được làm vô cùng sắc nét và tinh tế.\r\n\r\nMẫu quả cầu tuyết này có số lượng giới hạn nên hãy nhanh tay đặt hàng nhé.', 2, 0),
+(19, 'Đèn hình con heo', 'denconheo.jpg', 4500000, 100, 'Quả cầu tuyết giáng sinh ấm áp có kích thước 12 x 16 cm.\r\n\r\nPhát nhạc bằng cót, có đèn đổi màu ở cả chân đế và quả cầu.\r\n\r\nKhi phát nhạc, nội thất trong quả cầu có thể xoay tròn và tuyết tự phun mà không cần phải lắc.\r\n\r\nĐây là mẫu quả cầu tuyết cao cấp nên các chi tiết đều được làm vô cùng sắc nét và tinh tế.\r\n\r\nMẫu quả cầu tuyết này có số lượng giới hạn nên hãy nhanh tay đặt hàng nhé.', 2, 0),
+(20, 'Đèn led trái tim', 'denled.jpg', 500000, 80, 'Quả cầu tuyết giáng sinh ấm áp có kích thước 12 x 16 cm.\r\n\r\nPhát nhạc bằng cót, có đèn đổi màu ở cả chân đế và quả cầu.\r\n\r\nKhi phát nhạc, nội thất trong quả cầu có thể xoay tròn và tuyết tự phun mà không cần phải lắc.\r\n\r\nĐây là mẫu quả cầu tuyết cao cấp nên các chi tiết đều được làm vô cùng sắc nét và tinh tế.\r\n\r\nMẫu quả cầu tuyết này có số lượng giới hạn nên hãy nhanh tay đặt hàng nhé.', 2, 0),
+(21, 'Đèn lồng kính', 'denlongkinh.jpg', 4500000, 99, 'Quả cầu tuyết giáng sinh ấm áp có kích thước 12 x 16 cm.\r\n\r\nPhát nhạc bằng cót, có đèn đổi màu ở cả chân đế và quả cầu.\r\n\r\nKhi phát nhạc, nội thất trong quả cầu có thể xoay tròn và tuyết tự phun mà không cần phải lắc.\r\n\r\nĐây là mẫu quả cầu tuyết cao cấp nên các chi tiết đều được làm vô cùng sắc nét và tinh tế.\r\n\r\nMẫu quả cầu tuyết này có số lượng giới hạn nên hãy nhanh tay đặt hàng nhé.', 2, 1),
+(22, 'Đèn mặt trăng', 'denmattrang.jpg', 500000, 87, 'Quả cầu tuyết giáng sinh ấm áp có kích thước 12 x 16 cm.\r\n\r\nPhát nhạc bằng cót, có đèn đổi màu ở cả chân đế và quả cầu.\r\n\r\nKhi phát nhạc, nội thất trong quả cầu có thể xoay tròn và tuyết tự phun mà không cần phải lắc.\r\n\r\nĐây là mẫu quả cầu tuyết cao cấp nên các chi tiết đều được làm vô cùng sắc nét và tinh tế.\r\n\r\nMẫu quả cầu tuyết này có số lượng giới hạn nên hãy nhanh tay đặt hàng nhé.', 2, 3),
+(23, 'Khung ảnh xinh', 'khunganhdep.jpg', 50000, 100, 'Quả cầu tuyết giáng sinh ấm áp có kích thước 12 x 16 cm.\r\n\r\nPhát nhạc bằng cót, có đèn đổi màu ở cả chân đế và quả cầu.\r\n\r\nKhi phát nhạc, nội thất trong quả cầu có thể xoay tròn và tuyết tự phun mà không cần phải lắc.\r\n\r\nĐây là mẫu quả cầu tuyết cao cấp nên các chi tiết đều được làm vô cùng sắc nét và tinh tế.\r\n\r\nMẫu quả cầu tuyết này có số lượng giới hạn nên hãy nhanh tay đặt hàng nhé.', 4, 0),
+(24, 'Khung ảnh giáng sinh', 'khunganhgiangsinh.jpg', 50000, 100, 'Quả cầu tuyết giáng sinh ấm áp có kích thước 12 x 16 cm.\r\n\r\nPhát nhạc bằng cót, có đèn đổi màu ở cả chân đế và quả cầu.\r\n\r\nKhi phát nhạc, nội thất trong quả cầu có thể xoay tròn và tuyết tự phun mà không cần phải lắc.\r\n\r\nĐây là mẫu quả cầu tuyết cao cấp nên các chi tiết đều được làm vô cùng sắc nét và tinh tế.\r\n\r\nMẫu quả cầu tuyết này có số lượng giới hạn nên hãy nhanh tay đặt hàng nhé.', 4, 0),
+(25, 'Khung ảnh vẻ chân dung', 'khunganhlucky.jpg', 50000, 90, 'Quả cầu tuyết giáng sinh ấm áp có kích thước 12 x 16 cm.\r\n\r\nPhát nhạc bằng cót, có đèn đổi màu ở cả chân đế và quả cầu.\r\n\r\nKhi phát nhạc, nội thất trong quả cầu có thể xoay tròn và tuyết tự phun mà không cần phải lắc.\r\n\r\nĐây là mẫu quả cầu tuyết cao cấp nên các chi tiết đều được làm vô cùng sắc nét và tinh tế.\r\n\r\nMẫu quả cầu tuyết này có số lượng giới hạn nên hãy nhanh tay đặt hàng nhé.', 4, 0),
+(26, 'Khung ảnh cổ điển', 'khungcodien.jpg', 50000, 100, 'Quả cầu tuyết giáng sinh ấm áp có kích thước 12 x 16 cm.\r\n\r\nPhát nhạc bằng cót, có đèn đổi màu ở cả chân đế và quả cầu.\r\n\r\nKhi phát nhạc, nội thất trong quả cầu có thể xoay tròn và tuyết tự phun mà không cần phải lắc.\r\n\r\nĐây là mẫu quả cầu tuyết cao cấp nên các chi tiết đều được làm vô cùng sắc nét và tinh tế.\r\n\r\nMẫu quả cầu tuyết này có số lượng giới hạn nên hãy nhanh tay đặt hàng nhé.', 4, 0),
+(27, 'Khung ảnh gỗ nâu', 'khunggo.jpg', 50000, 100, 'Quả cầu tuyết giáng sinh ấm áp có kích thước 12 x 16 cm.\r\n\r\nPhát nhạc bằng cót, có đèn đổi màu ở cả chân đế và quả cầu.\r\n\r\nKhi phát nhạc, nội thất trong quả cầu có thể xoay tròn và tuyết tự phun mà không cần phải lắc.\r\n\r\nĐây là mẫu quả cầu tuyết cao cấp nên các chi tiết đều được làm vô cùng sắc nét và tinh tế.\r\n\r\nMẫu quả cầu tuyết này có số lượng giới hạn nên hãy nhanh tay đặt hàng nhé.', 4, 0),
+(28, 'Khung ảnh gỗ trắng', 'khunggo2.jpg', 50000, 100, 'Quả cầu tuyết giáng sinh ấm áp có kích thước 12 x 16 cm.\r\n\r\nPhát nhạc bằng cót, có đèn đổi màu ở cả chân đế và quả cầu.\r\n\r\nKhi phát nhạc, nội thất trong quả cầu có thể xoay tròn và tuyết tự phun mà không cần phải lắc.\r\n\r\nĐây là mẫu quả cầu tuyết cao cấp nên các chi tiết đều được làm vô cùng sắc nét và tinh tế.\r\n\r\nMẫu quả cầu tuyết này có số lượng giới hạn nên hãy nhanh tay đặt hàng nhé.', 4, 0),
+(29, 'Cốc hai màu', 'coc2mau.jpg', 100000, 100, 'Quả cầu tuyết giáng sinh ấm áp có kích thước 12 x 16 cm.\r\n\r\nPhát nhạc bằng cót, có đèn đổi màu ở cả chân đế và quả cầu.\r\n\r\nKhi phát nhạc, nội thất trong quả cầu có thể xoay tròn và tuyết tự phun mà không cần phải lắc.\r\n\r\nĐây là mẫu quả cầu tuyết cao cấp nên các chi tiết đều được làm vô cùng sắc nét và tinh tế.\r\n\r\nMẫu quả cầu tuyết này có số lượng giới hạn nên hãy nhanh tay đặt hàng nhé.', 1, 0),
+(30, 'Cốc đổi màu', 'cocdoimau.jpg', 100000, 80, 'Quả cầu tuyết giáng sinh ấm áp có kích thước 12 x 16 cm.\r\n\r\nPhát nhạc bằng cót, có đèn đổi màu ở cả chân đế và quả cầu.\r\n\r\nKhi phát nhạc, nội thất trong quả cầu có thể xoay tròn và tuyết tự phun mà không cần phải lắc.\r\n\r\nĐây là mẫu quả cầu tuyết cao cấp nên các chi tiết đều được làm vô cùng sắc nét và tinh tế.\r\n\r\nMẫu quả cầu tuyết này có số lượng giới hạn nên hãy nhanh tay đặt hàng nhé.', 1, 0),
+(31, 'Cốc hải tặc', 'cochaitac.jpg', 100000, 100, 'Quả cầu tuyết giáng sinh ấm áp có kích thước 12 x 16 cm.\r\n\r\nPhát nhạc bằng cót, có đèn đổi màu ở cả chân đế và quả cầu.\r\n\r\nKhi phát nhạc, nội thất trong quả cầu có thể xoay tròn và tuyết tự phun mà không cần phải lắc.\r\n\r\nĐây là mẫu quả cầu tuyết cao cấp nên các chi tiết đều được làm vô cùng sắc nét và tinh tế.\r\n\r\nMẫu quả cầu tuyết này có số lượng giới hạn nên hãy nhanh tay đặt hàng nhé.', 1, 0),
+(32, 'Cốc mèo', 'cocmeo.jpg', 100000, 100, 'Quả cầu tuyết giáng sinh ấm áp có kích thước 12 x 16 cm.\r\n\r\nPhát nhạc bằng cót, có đèn đổi màu ở cả chân đế và quả cầu.\r\n\r\nKhi phát nhạc, nội thất trong quả cầu có thể xoay tròn và tuyết tự phun mà không cần phải lắc.\r\n\r\nĐây là mẫu quả cầu tuyết cao cấp nên các chi tiết đều được làm vô cùng sắc nét và tinh tế.\r\n\r\nMẫu quả cầu tuyết này có số lượng giới hạn nên hãy nhanh tay đặt hàng nhé.', 1, 0),
+(33, 'Cốc nốt nhạc', 'cocnotnhac.jpg', 100000, 100, 'Quả cầu tuyết giáng sinh ấm áp có kích thước 12 x 16 cm.\r\n\r\nPhát nhạc bằng cót, có đèn đổi màu ở cả chân đế và quả cầu.\r\n\r\nKhi phát nhạc, nội thất trong quả cầu có thể xoay tròn và tuyết tự phun mà không cần phải lắc.\r\n\r\nĐây là mẫu quả cầu tuyết cao cấp nên các chi tiết đều được làm vô cùng sắc nét và tinh tế.\r\n\r\nMẫu quả cầu tuyết này có số lượng giới hạn nên hãy nhanh tay đặt hàng nhé.', 1, 0),
+(34, 'Cốc thỏ', 'coctho.jpg', 100000, 90, 'Quả cầu tuyết giáng sinh ấm áp có kích thước 12 x 16 cm.\r\n\r\nPhát nhạc bằng cót, có đèn đổi màu ở cả chân đế và quả cầu.\r\n\r\nKhi phát nhạc, nội thất trong quả cầu có thể xoay tròn và tuyết tự phun mà không cần phải lắc.\r\n\r\nĐây là mẫu quả cầu tuyết cao cấp nên các chi tiết đều được làm vô cùng sắc nét và tinh tế.\r\n\r\nMẫu quả cầu tuyết này có số lượng giới hạn nên hãy nhanh tay đặt hàng nhé.', 1, 0);
 
 -- --------------------------------------------------------
 
@@ -204,8 +225,11 @@ CREATE TABLE `users` (
 
 INSERT INTO `users` (`id`, `idRoles`, `username`, `password`, `name`, `address`, `phone`) VALUES
 (9, 0, 'thanh', 'd41d8cd98f00b204e9800998ecf8427e', 'Thanh Le', 'KTX KHU B ĐHQG, Phường Linh Trung, Quận Thủ Đức, TP. Hồ Chí Minh', '866571001'),
-(11, 0, 'locchiu', '202cb962ac59075b964b07152d234b70', 'Phú lộc', 'tt', '866571001'),
-(12, 0, 'user01', '202cb962ac59075b964b07152d234b70', 'user 01', 'userr 12344', '0123');
+(11, 1, 'locchiu', '202cb962ac59075b964b07152d234b70', 'Phú lộc', 'tt', '866571001'),
+(12, 1, 'user01', '202cb962ac59075b964b07152d234b70', 'user 01', 'userr 12344', '0123'),
+(13, 1, 'user02', '202cb962ac59075b964b07152d234b70', 'Le Van Thanh', '24144', '123'),
+(17, 0, 'user04', '202cb962ac59075b964b07152d234b70', 'thanh', 'levanthan 12345', '123666'),
+(21, 1, 'admin', '202cb962ac59075b964b07152d234b70', 'Admin', 'Admin', '0123556');
 
 --
 -- Chỉ mục cho các bảng đã đổ
@@ -278,19 +302,19 @@ ALTER TABLE `users`
 -- AUTO_INCREMENT cho bảng `bill`
 --
 ALTER TABLE `bill`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=23;
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=28;
 
 --
 -- AUTO_INCREMENT cho bảng `billdetails`
 --
 ALTER TABLE `billdetails`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=13;
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=20;
 
 --
 -- AUTO_INCREMENT cho bảng `cart`
 --
 ALTER TABLE `cart`
-  MODIFY `Id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=23;
+  MODIFY `Id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=38;
 
 --
 -- AUTO_INCREMENT cho bảng `danhsach`
@@ -302,13 +326,13 @@ ALTER TABLE `danhsach`
 -- AUTO_INCREMENT cho bảng `loaisanpham`
 --
 ALTER TABLE `loaisanpham`
-  MODIFY `maLoai` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=6;
+  MODIFY `maLoai` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=8;
 
 --
 -- AUTO_INCREMENT cho bảng `sanpham`
 --
 ALTER TABLE `sanpham`
-  MODIFY `maSP` bigint(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=5;
+  MODIFY `maSP` bigint(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=36;
 
 --
 -- AUTO_INCREMENT cho bảng `slide`
@@ -320,7 +344,7 @@ ALTER TABLE `slide`
 -- AUTO_INCREMENT cho bảng `users`
 --
 ALTER TABLE `users`
-  MODIFY `id` bigint(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=13;
+  MODIFY `id` bigint(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=22;
 
 --
 -- Các ràng buộc cho các bảng đã đổ
